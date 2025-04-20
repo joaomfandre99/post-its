c729e170bdd282fa70e09721b927938bd25e5e02
JoaoMFAndre <joaoandre@dynamik.pt>
2025-04-20 15:08:41 +0100

    feat: writeable textarea that fits viewport window

---
+++ src/renderer/App.tsx
@@ -0,0 +1,13 @@
+import { BaseSyntheticEvent } from 'react';
+import './index.css';
+
+const App = () => {
+
+    return (
+        <>
+        <textarea id='note' name='note' placeholder='Type your note...'>
+        </textarea>
+        </>
+    )
+}
+export default App;
